07b0bd7bc88e554f29f15397723c33000186936c
Laurentiu Tusa <lautusa@yahoo.com>
2023-04-01 15:32:46 +0300

    Initial commit

---
+++ src/com/company/Product.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Product {
+
+    private String Name;
+    private int price;
+
+    public Product(String name, int price) {
+        Name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
